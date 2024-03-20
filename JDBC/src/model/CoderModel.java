4c32c417661d03fdb2eb14ce9c2d3f9119d38bb6
Dayron-z <zbastian.zapata@gmail.com>
2024-03-20 07:38:37 -0500

    Inyeccion de datos casi finalizada

--- JDBC/src/model/CoderModel.java
+++ JDBC/src/model/CoderModel.java
@@ -20,7 +20,67 @@ public class CoderModel implements CRUD {
 
     @Override
     public Object insert(Object obj) {
-        return null;
+
+        //Abrimos conexión
+        Connection objConecction = ConfigDB.openConnection();
+
+
+        //2 Convertir el obj que llegó a Coder
+        Coder objCoder = (Coder) obj;
+
+
+
+        //El obj que nos llega lo debemos convertir a coder, ya que necesitamos acceder a sus atributos, para poder hacer la inyección de datos
+
+
+
+
+        try {
+            //Tengamos en cuenta que el funcionamiento de esto es muy sencillo, cuando nosotros ejecutamos codigo sql, se hace lo que le pasamos al String SQl, y luego le pasamos los valores de forma dinamica.
+
+
+            String sql = "INSERT INTO coder (name,age,clan) VALUES (?,?,?);";
+            PreparedStatement objPrepare = objConecction.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            //Asignamos los valores desde el objPrepare
+            objPrepare.setString(1, objCoder.getName());
+            objPrepare.setInt(2, objCoder.getAge());
+            objPrepare.setString(3, objCoder.getClan());
+            //Hasta este punto es como si hubieramos creado la parte mysql que inserta datos dentro de una tabla.
+
+            //Ahora debemos proceder a ejecutar
+            //Execute lo debemos tomar simplemenete como que hace la ejecución y listo, en este caso no necesitamos guardar lo que nos proporcione, solo que ejecute
+            objPrepare.execute();
+
+            //7. Obtener el resultado con los id (LLaves generadas)
+            //Guardamos
+            ResultSet objRest = objPrepare.getGeneratedKeys();
+
+            //8. Iterar mientras haya un registro
+            // Esta validacion se suele hacer cuando necesitamos recuperar datos de la base de datos
+                //En este caso necesitamos recuperar el id auto incremental que nos proporciona mySQL.
+            while (objRest.next()){
+                // Nota kevin: Podemos obtener el valor tambien con indices
+                //Lo haremos con nombre
+                objCoder.setId(objRest.getInt("id"));
+            }
+
+            JOptionPane.showMessageDialog(null, "Se agregaron datos satisfactoriamente");
+
+
+
+
+
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error " + e.getMessage());
+        }
+
+
+
+        //Error dejado a propósito
+        //Estudiar el retorno
+        //La respuesta debe ser analizada junto con el controller.
+        return objCod;
     }
 
     //Nos retornará una lista con el tipo de dato object, es decir, lo usamos con el fin de que nos retorne multiples datos y parsearlos o castearlos a nuestra conveniencia
